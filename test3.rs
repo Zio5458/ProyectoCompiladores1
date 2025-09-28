132e7dd0b4d474dcaddd498d88a0f24976110c8b
Zio5458 <hdannysabillon992232@gmail.com>
2025-09-27 22:21:35 -0600

    pocos cambios a los tests y limpieza del repositorio

--- test3.rs
+++ test3.rs
@@ -1,15 +1,11 @@
-fn test_operators() -> i32 {
-    let a = 10 + 5;
-    let b = a - 3;
-    let c = b * 2;
-    let d = c / 4;
-    let e = d % 3;
-    return e;
+fn test_primitives() -> i32 {
+    let a = 42;
+    let b = 3;
+    let c = a + b;
+    return c;
 }
 
-fn test_logic() -> bool {
-    let a = true && false;
-    let b = true || false;
-    let c = !a;
-    return c;
+fn test_bool() -> bool {
+    let flag = true;
+    return flag;
 }
